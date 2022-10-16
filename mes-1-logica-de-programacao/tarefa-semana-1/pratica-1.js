91e56fc8fe223c1bdb805dc7b8cc235d0c386705
Raphael <raphael.depaula@hotmail.com>
2022-10-16 16:19:40 -0300

    add pratica1

---
+++ mes-1-logica-de-programacao/tarefa-semana-1/pratica-1.js
@@ -0,0 +1,11 @@
+let  nome = 'Raphael'
+let  email = 'raphael.depaula@hotmail.com'
+let  profissao = 'administrativo'
+let  idade = 31
+let  lugarquevocesonhaumdiaconhecer = 'Italia'
+
+console.log(typeof nome)
+console.log(typeof email)
+console.log(typeof profissao)
+console.log(typeof idade)
+console.log(typeof lugarquevocesonhaumdiaconhecer)
